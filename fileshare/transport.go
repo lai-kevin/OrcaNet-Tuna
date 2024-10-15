c5b46f464a22819722708a7b2af5cccef9d51353
Kevin Lai <kevinlaisoftware@gmail.com>
2024-10-15 00:00:05 -0400

    implement sendFileToPeer

--- fileshare/transport.go
+++ fileshare/transport.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"os"
 
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
@@ -35,7 +37,59 @@ func findPeerAndConnect(context context.Context, orcaDHT *dht.IpfsDHT, node host
 	return fullMultiAddress, nil
 }
 
+// Send a file to a peer from a given node. This function assumes peer is already connected. Use in conjunction with findPeerAndConnect.
+func sendFileToPeer(context context.Context, fullPeerMultiAddress string, node host.Host, filepath string) (err error) {
+	// Parse the peerID from the multiaddress
+	decodedPeerID, err := peer.Decode(fullPeerMultiAddress)
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to decode peerID: %v", err)
+	}
+
+	// Create a new stream to the target peer
+	stream, err := node.NewStream(context, decodedPeerID, "/fileshare/sendFile")
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to open stream: %v", err)
+	}
+	defer stream.Close()
+
+	// Get the file size
+	fileInfo, err := os.Stat(filepath)
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to get file size: %v", err)
+	}
+	fileSize := fileInfo.Size()
+
+	// Write the file size to the stream
+	fileSizeBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(fileSizeBytes, uint64(fileSize))
+	_, err = stream.Write(fileSizeBytes)
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to write file size to stream: %v", err)
+	}
+
+	// Open the file and write it to the stream
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(stream, file)
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to write file to stream: %v", err)
+	}
+
+	// Close the stream
+	err = stream.Close()
+	if err != nil {
+		return fmt.Errorf("sendFileToPeer: failed to close stream: %v", err)
+	}
+
+	return nil
+}
+
 // Send chunks to a peer from a given node. This function assumes peer is already connected.
+// WARNGING: THIS FUNCTION IS NOT TESTED AND MAY NOT BE SUPPORTED IN THE FINAL VERSION.
 func sendChunksToPeer(context context.Context, fullPeerMultiAddress string, node host.Host, chunks []Chunk) (err error) {
 	// Parse peer peerID from multiaddress
 	decodedPeerID, err := peer.Decode(fullPeerMultiAddress)
@@ -46,6 +100,13 @@ func sendChunksToPeer(context context.Context, fullPeerMultiAddress string, node
 		return fmt.Errorf("failed to open stream: %v", err)
 	}
 	defer stream.Close()
+
+	totalBytes := 0
+	for _, chunk := range chunks {
+		totalBytes += len(chunk.Data)
+	}
+
+	transferredBytes := 0
 
 	// Send chunks to peer
 	for _, chunk := range chunks {
@@ -65,11 +126,18 @@ func sendChunksToPeer(context context.Context, fullPeerMultiAddress string, node
 			return fmt.Errorf("sendChunksToPeer: failed to write chunk data length to stream: %v", err)
 		}
 
+		// Write total bytes to be transferred
+		totalBytesBytes := make([]byte, 4)
+		binary.BigEndian.PutUint32(totalBytesBytes, uint32(totalBytes))
+
 		// Write the chunk data
 		_, err = stream.Write(chunk.Data)
 		if err != nil {
 			return fmt.Errorf("failed to write to stream: %v", err)
 		}
+
+		// Update the number of transferred bytes
+		transferredBytes += len(chunk.Data)
 	}
 
 }
